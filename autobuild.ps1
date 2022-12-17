2861ff0da3aaef101b01aa3c3c668a40cd329655
KevinMatt <he_yuheng@163.com>
2022-12-17 08:18:38 +0800

    Update Blog

--- autobuild.ps1
+++ autobuild.ps1
@@ -1,5 +1,5 @@
 hugo
-git add . && git commit -m "Update Blog" && git push
 cd public
 git add . && git commit -m "Update Blog" && git push
 cd ..
+git add . && git commit -m "Update Blog" && git push
